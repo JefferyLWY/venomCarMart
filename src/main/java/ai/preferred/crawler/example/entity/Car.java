9de08cad3932c360ad23662613a1919742d7669e
admin <admin@192.168.11.158>
2018-10-04 13:07:01 +0800

    Updated Car

--- src/main/java/ai/preferred/crawler/example/entity/Car.java
+++ src/main/java/ai/preferred/crawler/example/entity/Car.java
@@ -10,12 +10,207 @@ package ai.preferred.crawler.example.entity;
  * @author admin
  */
 public class Car {
-    private final String name;
-    private final String url;
-    private final int price;
-    private final int deprecation;
-            
-    public Car() {
+    private  String name;
+    private  String url;
+    private  String price;
+    private  String depreciation;
+    private  String regDate;
+    private  String manufactured;
+    private  String mileage;
+    private  String transmission;
+    private  String engineCap;
+    private  String roadTax;
+    private  String power;
+    private  String curbWeight;
+    private  String features;
+    private  String accessories;
+    private  String description;
+    private  String coe;
+    private  String omv;
+    private  String arf;
+    private  String deregValue;
+    private  String noOfOwners;
+    private  String typeOfOVeh;
+    private  String category;
+
+    public Car(){
         
     }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDepreciation() {
+        return depreciation;
+    }
+
+    public void setDepreciation(String depreciation) {
+        this.depreciation = depreciation;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getManufactured() {
+        return manufactured;
+    }
+
+    public void setManufactured(String manufactured) {
+        this.manufactured = manufactured;
+    }
+
+    public String getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(String mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getEngineCap() {
+        return engineCap;
+    }
+
+    public void setEngineCap(String engineCap) {
+        this.engineCap = engineCap;
+    }
+
+    public String getRoadTax() {
+        return roadTax;
+    }
+
+    public void setRoadTax(String roadTax) {
+        this.roadTax = roadTax;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+
+    public String getCurbWeight() {
+        return curbWeight;
+    }
+
+    public void setCurbWeight(String curbWeight) {
+        this.curbWeight = curbWeight;
+    }
+
+    public String getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(String features) {
+        this.features = features;
+    }
+
+    public String getAccessories() {
+        return accessories;
+    }
+
+    public void setAccessories(String accessories) {
+        this.accessories = accessories;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCoe() {
+        return coe;
+    }
+
+    public void setCoe(String coe) {
+        this.coe = coe;
+    }
+
+    public String getOmv() {
+        return omv;
+    }
+
+    public void setOmv(String omv) {
+        this.omv = omv;
+    }
+
+    public String getArf() {
+        return arf;
+    }
+
+    public void setArf(String arf) {
+        this.arf = arf;
+    }
+
+    public String getDeregValue() {
+        return deregValue;
+    }
+
+    public void setDeregValue(String deregValue) {
+        this.deregValue = deregValue;
+    }
+
+    public String getNoOfOwners() {
+        return noOfOwners;
+    }
+
+    public void setNoOfOwners(String noOfOwners) {
+        this.noOfOwners = noOfOwners;
+    }
+
+    public String getTypeOfOVeh() {
+        return typeOfOVeh;
+    }
+
+    public void setTypeOfOVeh(String typeOfOVeh) {
+        this.typeOfOVeh = typeOfOVeh;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
 }
